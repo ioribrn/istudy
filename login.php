58a8bf0079e84a103911ee9f474ab94796a35ee2
Abdennour Jebbar <35144705+Shadow13X@users.noreply.github.com>
2019-04-30 15:07:44 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -14,7 +14,9 @@
     <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
     <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Theme Style -->
     <link rel="stylesheet" href="css/style.css">
   </head>
@@ -22,9 +24,9 @@
     
     <header role="banner">
      
-      <nav class="navbar navbar-expand-md navbar-dark bg-light">
-        <div class="container">
-          <a class="navbar-brand absolute" href="index.php"><img src="./images/iStudy-logo-brand.png"></img></a>
+      <nav class="navbar navbar-expand-md navbar-dark bg-light" style="background-color:#00989f;">
+        <div class="container" style="background-color:#00989f;" >
+          <a class="navbar-brand absolute" href="index.php"><img src="././images/iStudy-logo-brand.png"></img></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -82,26 +84,69 @@
     </header>
     <!-- END header -->
 
-    <section class="site-hero site-hero-innerpage overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/big_image_1.jpg);">
+    <section class="site-hero overlay" data-stellar-background-ratio="0" style="height:10px;">
       <div class="container">
         <div class="row align-items-center site-hero-inner justify-content-center">
           <div class="col-md-8 text-center">
 
-            <div class="mb-5 element-animate">
-              <h1>Login</h1>
+            <div id="login" class="mb-5 element-animate">
+              <h1 style="color:#00989f">Login</h1>
             </div>
           </div>
         </div>
       </div>
+      <div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-1">
+                    <h3>Student Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd">Forget Password?</a>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6 login-form-2">
+                    <h3>Tutor Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div class="form-group">
+
+                            <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+          </div>
+        </div>
+</div>
     </section>
+    <div class="container">
+</div>
+
+
     <!-- END section -->
 
 
-    <section class="site-section bg-light">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-5 box">
-            <h2 class="mb-5">Log in with your account</h2>
+    
+            <!--<h2 class="mb-5">Log in with your account</h2>
             <form action="#" method="post">
                   <div class="row">
                     <div class="col-md-12 form-group">
@@ -121,18 +166,16 @@
                       <input type="submit" value="Login" class="btn btn-primary">
                     </div>
                   </div>
-                </form>
-          </div>
-        </div>
-      </div>
-    </section>
+                </form>-->
+          
+
     <!-- END section -->
 
     
     <!-- END section -->
     
   
-    <footer class="site-footer" style="background-image: url(images/big_image_3.jpg);">
+    <footer class="site-footer" style="background-image: url(./images/big_image_3.jpg);">
       <div class="container">
         <div class="row mb-5">
           <div class="col-md-4">
